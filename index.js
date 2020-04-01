a81329b2378ac1cf626b6f8f6ab091c7980c42c7
kalmegati <kalmegati@gmail.com>
2020-04-01 12:32:33 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(people, occasion) {
+    let cards = [];
+    for (let i=0; i < people.length; i++) {
+        cards.push(`Thank you, ${people[i]}, for the wonderful ${occasion} gift!`);
+    }
+    return cards;
+}
+
+function countDown(peak) {
+    while (peak >= 0) {
+        console.log(peak)
+        peak--
+    }
+}
